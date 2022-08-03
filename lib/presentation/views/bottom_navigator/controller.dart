23a9581709facbd65377305ce8d40b9a5e490936
Sourav Singh Rawat <souravrawat@Souravs-MacBook-Air.local>
2022-08-03 17:25:34 +0530

    added bottom navigator

---
+++ lib/presentation/views/bottom_navigator/controller.dart
@@ -0,0 +1,39 @@
+part of './view.dart';
+
+final _vsProvider = StateNotifierProvider.autoDispose<_VSController, _ViewState>(
+  (ref) {
+    final stateController = _VSController();
+    return stateController;
+  },
+);
+
+class _ViewState {
+  final int currentIndex;
+
+  _ViewState({
+    required this.currentIndex,
+  });
+
+  _ViewState.initState()
+      : this(
+          currentIndex: 0,
+        );
+
+  _ViewState copyWith({
+    int? currentIndex,
+  }) {
+    return _ViewState(
+      currentIndex: currentIndex ?? this.currentIndex,
+    );
+  }
+}
+
+class _VSController extends StateNotifier<_ViewState> {
+  _VSController() : super(_ViewState.initState());
+
+  void onIndexChanged(int index) {
+    state = state.copyWith(
+      currentIndex: index,
+    );
+  }
+}
